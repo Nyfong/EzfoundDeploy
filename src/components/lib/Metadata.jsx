8ce7e3c4ee88a434cd1ca7eeb07f587f7fb86b10
Nyfong <litongfong12@gmail.com>
2024-09-27 10:52:14 +0700

    deploytoda6

---
+++ src/components/lib/Metadata.jsx
@@ -0,0 +1,39 @@
+// Metadata.jsx
+import React from "react";
+import { Helmet } from "react-helmet-async";
+export const Metadata = ({
+  title,
+  description,
+  author,
+  keywords,
+  thumbnail,
+}) => {
+  return (
+    <Helmet>
+      <title>{`CSTAD | ${title || "Easy Found"}`}</title>
+      <meta name="title" content={title || "Easy Found"} />
+      <meta
+        name="description"
+        content={description || "Add default description here"}
+      />
+      <meta name="author" content={author} />
+      <meta name="keywords" content={keywords || "Add default keywords here"} />
+      <meta
+        name="thumbnail"
+        content={thumbnail || "Add default thumbnail here"}
+      />
+      <meta property="og:url" content={"example.url"} />
+      <meta property="og:type" content={"ServiceListing website"} />
+      <meta property="og:title" content={title || "Easy Found"} />
+      <meta
+        property="og:description"
+        content={description || "Add default description here"}
+      />
+      <meta
+        property="og:image"
+        content={thumbnail || "Add default thumbnail here"}
+      />
+      // you can add more meta to imporove SEO
+    </Helmet>
+  );
+};
